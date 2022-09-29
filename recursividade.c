ff284cd8e996eb602821d0670d84df59a8215652
TiagoAlex02 <tiagoalexandre@usp.br>
2022-09-29 08:34:43 -0300

    Fibonacci com Recursividade

---
+++ recursividade.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int n;
+int fib(n){
+    if(n==1){
+        return 0;
+    }
+    if(n==2){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+int main(void){
+    int n;
+    scanf("%d",&n);
+    printf("%d",fib(n));
+    return 0;
+}
