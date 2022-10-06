b6829b1d938c31b1af3bf6fe9d63e34c19292ee0
TiagoAlex02 <87451888+TiagoAlex02@users.noreply.github.com>
2022-10-06 15:09:16 -0300

    Arquivo com o código

---
+++ Fibonacci Recursivo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int i;
+int fib_recursiva(i){
+    if(i==1){
+        return 0;
+    }
+    if(i==2){
+        return 1;
+    }
+    return fib_recursiva(n-2) + fib_recursiva(n-1);
+}
+
+int main(void){
+    int i;
+    scanf("%d",&i);
+    printf("%d",fib_recursiva(i));
+    return 0;
+}
